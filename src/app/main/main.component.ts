498506511007b1be26e3713c3b05e4d74b2d2dc4
CAT <bigstirnabon@gmail.com>
2024-03-02 23:49:01 +0700

    Update main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -24,25 +24,57 @@ ngOnInit(): void {
 
 
 async getPicture() {
+
+  let lastRandomIndex1: number | null = null;
+  let lastRandomIndex2: number | null = null;
+  let lastRandomData1: any;
+  let lastRandomData2: any;
+  let lastRandomTime: number | null = null;
+
 const url = 'http://localhost:3000/pictrue/all';
 const data = await lastValueFrom(
 this.http.get(url));
 this.Picture = data as PictureGetResponse[];
 console.log(this.Picture);
-const randomIndex1 = Math.floor(Math.random() * this.Picture.length);
-let randomIndex2 = Math.floor(Math.random() * this.Picture.length);
+const currentTime = new Date().getTime();
 
-while (randomIndex2 === randomIndex1) {
-  randomIndex2 = Math.floor(Math.random() * this.Picture.length);
-}
-console.log(randomIndex1);
-console.log(randomIndex2);
+    if (!lastRandomTime || currentTime - lastRandomTime >= 10000) {
+        let randomIndex1 = Math.floor(Math.random() * this.Picture.length);
+        let randomIndex2 = Math.floor(Math.random() * this.Picture.length);
 
-const randomData1 = this.Picture[randomIndex1];
-console.log(randomData1);
-const randomData2 = this.Picture[randomIndex2];
-console.log(randomData2);
+        while (randomIndex2 === randomIndex1) {
+            randomIndex2 = Math.floor(Math.random() * this.Picture.length);
+        }
+
+        lastRandomIndex1 = randomIndex1;
+        lastRandomIndex2 = randomIndex2;
+        lastRandomData1 = this.Picture[randomIndex1];
+        lastRandomData2 = this.Picture[randomIndex2];
+        lastRandomTime = currentTime;
+
+        console.log(lastRandomIndex1, lastRandomIndex2);
+        console.log(lastRandomData1, lastRandomData2);
+    } else {
+        let randomIndex1 = Math.floor(Math.random() * this.Picture.length);
+        let randomIndex2 = Math.floor(Math.random() * this.Picture.length);
+
+        while (randomIndex1 === lastRandomIndex1 || randomIndex2 === lastRandomIndex2 || randomIndex2 === randomIndex1) {
+            randomIndex2 = Math.floor(Math.random() * this.Picture.length);
+        }
+
+        lastRandomIndex1 = randomIndex1;
+        lastRandomIndex2 = randomIndex2;
+        lastRandomData1 = this.Picture[randomIndex1];
+        lastRandomData2 = this.Picture[randomIndex2];
+
+        console.log(lastRandomIndex1, lastRandomIndex2);
+        console.log(lastRandomData1, lastRandomData2);
+    }
+
+
 }
+
+
 
 
 }
