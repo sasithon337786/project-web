7698a6ba234a5be564e93489641a5747d84a7de9
sasithon337786 <161368508+sasithon337786@users.noreply.github.com>
2024-03-02 23:18:28 +0700

    hh

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,7 +10,12 @@ import { UserService } from '../services/api/user.service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, MatButtonModule,HttpClientModule],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    HttpClientModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -21,26 +26,28 @@ export class LoginComponent {
     private userService: UserService
   ) {}
 
-  
-  
   async login(email: string, password: string) {
     const url = 'http://localhost:3000/users';
     try {
       const data = await this.http.get(url).toPromise();
       const users = data as UserGetResponse[];
-      const foundUser = users.find(user => user.user_email === email && user.user_pass === password);
+      const foundUser = users.find(
+        (user) => user.user_email === email && user.user_pass === password
+      );
 
       if (foundUser) {
-        console.log("User found:", foundUser);
+        console.log('User found:', foundUser);
         if (password === foundUser.user_pass) {
-          localStorage.setItem("userID", JSON.stringify(foundUser.user_id));
-          console.log("Session ID : " + foundUser.user_id + " is set on LocalStorage");
+          localStorage.setItem('userID', JSON.stringify(foundUser.user_id));
+          console.log(
+            'Session ID : ' + foundUser.user_id + ' is set on LocalStorage'
+          );
           this.navigateTomain(); // Navigate to 'main' route
         } else {
-          alert("รหัสผ่านไม่ถูกต้อง");
+          alert('รหัสผ่านไม่ถูกต้อง');
         }
       } else {
-        alert("ไม่มีผู้ใช้นี้อยู่ในระบบ โปรดสมัครสมาชิก");
+        alert('ไม่มีผู้ใช้นี้อยู่ในระบบ โปรดสมัครสมาชิก');
       }
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -50,5 +57,3 @@ export class LoginComponent {
     this.router.navigate(['/main']);
   }
 }
-  
-
