392e2852b3634ecf3ef43419be9cd210120ad24d
sasithon337786 <161368508+sasithon337786@users.noreply.github.com>
2024-03-04 21:25:56 +0700

    Update user.service.ts

--- src/app/services/api/user.service.ts
+++ src/app/services/api/user.service.ts
@@ -10,6 +10,35 @@ import { lastValueFrom } from 'rxjs';
 export class UserService {
 
   user : UserGetResponse[] = [];
+  import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Constants } from '../../config/constants';
+import { UserGetResponse } from '../../model/user_get';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+
+  user : UserGetResponse[] = [];
+  userData : UserGetResponse | undefined;
+  constructor(private constants : Constants, private http: HttpClient ) {
+    this.getName();
+  }
+
+  async getName() {
+    const url = 'http://localhost:3000/users';
+    const data = await lastValueFrom(
+      this.http.get(url));
+    this.user = data as UserGetResponse[];
+    console.log(this.user);
+  }
+
+
+
+
+}
   constructor(private constants : Constants, private http: HttpClient ) {
     this.getName();
   }
